afac8b2afa3451f3b83da6e55545849f47f9e583
Suyeon <msjgr2801@gmail.com>
2024-05-10 16:55:38 +0900

    feat: SidebarHeader 컴포넌트 기본 메서드 구현

---
+++ src/components/SidebarHeader/index.ts
@@ -0,0 +1,26 @@
+class SidebarHeader {
+	$target: Element;
+	$sidebarHeader: HTMLDivElement | null;
+
+	constructor($target: Element) {
+		this.$target = $target;
+		this.$sidebarHeader = null;
+		this.init();
+		this.render();
+	}
+
+	init() {
+		this.$sidebarHeader = document.createElement('div');
+		this.$sidebarHeader.classList.add('sidebar__header');
+		this.$target.appendChild(this.$sidebarHeader);
+	}
+
+	render() {
+		if (this.$sidebarHeader) {
+			this.$sidebarHeader.innerHTML = `
+			`;
+		}
+	}
+}
+
+export default SidebarHeader;
