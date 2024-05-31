068ad8552f7beea3e764f616678a912d4cacb709
Suyeon <msjgr2801@gmail.com>
2024-05-31 14:24:15 +0900

    feat: Title 클릭하면 메인 페이지로 이동하도록 하기

--- src/components/SidebarHeader/index.ts
+++ src/components/SidebarHeader/index.ts
@@ -1,9 +1,10 @@
 import Component from '~/core/components/Component';
+import { navigate } from '~/core/router';
 
 class SidebarHeader extends Component {
   template(): string {
     return `
-      <div class='sidebar__container'>
+      <div data-link='/' class='sidebar__container'>
         <div class='sidebar__header-img'>
           <img src='/src/assets/propfile.png' />
         </div>
@@ -12,6 +13,20 @@ class SidebarHeader extends Component {
         </div>
       </div>
       `;
+  }
+
+  addEvent(): void {
+    this.$target.addEventListener('click', (event) => {
+      event.stopPropagation();
+
+      if (event.target instanceof HTMLDivElement) {
+        const { link } = event.target.dataset;
+
+        if (link) {
+          navigate(link);
+        }
+      }
+    });
   }
 }
 
