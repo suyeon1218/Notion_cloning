9301fc9fa55df1bab045010db6916efb4e4bb6ff
Suyeon <msjgr2801@gmail.com>
2024-07-02 15:53:11 +0900

    feat: request -> apiClient 파일로 이름 변경 및 엔드포인트 추가

---
+++ src/api/apiClient.ts
@@ -0,0 +1,43 @@
+export const notionAPIClient = async <T>(
+  path: string,
+  options?: Partial<RequestInit>
+): Promise<T | undefined> => {
+  try {
+    const response = await fetch(`/notion/documents/${path}`, {
+      ...options,
+      headers: {
+        'x-username': 'suyeon',
+      },
+    });
+
+    if (response.ok) {
+      const data: Promise<T> = response.json();
+
+      return data;
+    }
+    return undefined;
+  } catch (error) {
+    throw new Error('API ERROR');
+  }
+};
+
+export const emojiAPIClient = async <T>(
+  path: string,
+  options?: Partial<RequestInit>
+) => {
+  try {
+    const response = await fetch(`/emoji/${path}`, {
+      ...options,
+    });
+
+    if (response.ok) {
+      const data: Promise<T> = response.json();
+
+      return data;
+    }
+
+    return undefined;
+  } catch (error) {
+    throw new Error('API ERROR');
+  }
+};
