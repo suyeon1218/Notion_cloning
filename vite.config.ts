912b7c741a3ae91a6cef8e2f126cc44919b5ca2a
Suyeon <msjgr2801@gmail.com>
2024-07-02 16:07:28 +0900

    fix: emoji API 요청이 가지 않는 문제 수정

--- vite.config.ts
+++ vite.config.ts
@@ -9,15 +9,15 @@ export default ({ mode }) => {
     plugins: [tsconfigPaths()],
     server: {
       proxy: {
-        '/api': {
+        '/notion': {
           target: process.env.VITE_NOTION_API_END_POINT,
           changeOrigin: false,
           rewrite: (path) => path.replace(/^\/notion/, ''),
           secure: false,
         },
         '/emoji': {
-          target: process.env.VITE_EMOJI_API_ENO_POINT,
-          changeOrigin: false,
+          target: process.env.VITE_EMOJI_API_END_POINT,
+          changeOrigin: true,
           rewrite: (path) => path.replace(/^\/emoji/, ''),
           secure: false,
         },
