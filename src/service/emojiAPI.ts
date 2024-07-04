aeccdfb199c69c04f55941735b8f409bd7b8cca5
Suyeon <msjgr2801@gmail.com>
2024-07-04 17:32:03 +0900

    feat: 에러 타입 반환하기

--- src/service/emojiAPI.ts
+++ src/service/emojiAPI.ts
@@ -7,19 +7,29 @@ const baseQueryKey = {
   access_key: import.meta.env.VITE_EMOJI_API_KEY,
 };
 
+export interface EmojiError {
+  status: string;
+  message: string;
+}
+
 const emojiAPI = {
   getEmojiByGroup: async (groupKeyword: string) => {
     const emoji = await requestEmojiAPI.get<Emoji[]>(
-      `/${EMOJI.API.CATEGORY}/${groupKeyword}?${makeQueryString(baseQueryKey)}`
+      `${EMOJI.API.CATEGORY}/${groupKeyword}?${makeQueryString(baseQueryKey)}`
     );
 
     return emoji;
   },
-  // getEmojiBySubCategory: async (category) => {
-  //   const emoji = await makeRequest.get<Emoji[]>();
+  getEmojiByKeyword: async (keyword: string) => {
+    const emoji = requestEmojiAPI.get<Emoji[] | EmojiError>(
+      `${EMOJI.API.EMOJI}?${makeQueryString({
+        ...baseQueryKey,
+        search: keyword,
+      })}`
+    );
 
-  //   return emoji;
-  // },
+    return emoji;
+  },
 };
 
 export default emojiAPI;
